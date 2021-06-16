a05f54fcfcd022a3bdbb8718497c94aca4e14faf
Jason Nguyen <xtremejason@gmail.com>
2021-06-16 15:45:09 -0600

    Implementation of GarageEmployee abstract Superclass and Mechanic subclass

--- src/GarageEmployee.java
+++ src/GarageEmployee.java
@@ -1,3 +1,24 @@
+import java.util.Random;
+
 public abstract class GarageEmployee {
+
+    String name;
+    String[] names = {"Bobby", "Joe", "Andrea","Timothy","Audrey","Madison","Devin","Ashley","Brenda","Cody","AJ","Randy","Lisa","Steve","Will"};
+
+    //constructor sets name of employee
+    public void GarageEmployee(){
+        Random rand = new Random();
+        int randomNum = rand.nextInt(14);
+        name = names[randomNum];
+    }
+
+    //manually sets name
+    public void setName(String n){
+        name = n;
+    }
+
+    public void getName(){
+        System.out.println(name);
+    }
 
 }
--- src/main.java
+++ src/main.java
@@ -8,11 +8,13 @@ public class main {
     public static void main(String[] args) {
         //Vehicles test = new Vehicles('t');
         Bike test = new Bike();
-
         test.lock();
         test.displayPlate();
 
-        String[] names = {"Bobby", "Joe", "Andrea","Timothy","Audrey","Madison","Devin","Ashley","Brenda","Cody","AJ","Randy","Lisa","Steve","Will"};
+        //Testing for Mechanic -> GarageEmployee super/sub-class
+        Mechanic test2 = new Mechanic();
+        test2.GarageEmployee();
+        test2.getName();
 
 
 
