94d10fb84dacd882b3a2d606dad8c7e3c645f60a
Jason Nguyen <xtremejason@gmail.com>
2021-06-22 18:25:07 -0600

    Completed Assignment. Added additional comments & documentation.

--- src/main.java
+++ src/main.java
@@ -1,5 +1,4 @@
-//TEST Git Checkout Alternate Branch Functionality
-
+//TEST Git Checkout Alternate Branch Functionality.
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
