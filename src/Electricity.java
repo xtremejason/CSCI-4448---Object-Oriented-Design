e46980eac4c1370271d594dbd70c19daf1b42bf3
Jason Nguyen <xtremejason@gmail.com>
2021-06-23 02:14:46 -0600

    Manual merge of Jason's branch to master branch due to branch complications. (loss of some commit history)

---
+++ src/Electricity.java
@@ -0,0 +1,7 @@
+//Strategy Design Pattern for Vehicle's fuel type.
+
+public class Electricity implements FuelType{
+    public void printFuel(){
+        System.out.println("I use Electricity as fuel. Charge me up baby!");
+    }
+}
---
+++ src/FuelType.java
@@ -0,0 +1,5 @@
+//Strategy Design Pattern for Vehicle's fuel type.
+
+public interface FuelType {
+    public void printFuel();
+}
